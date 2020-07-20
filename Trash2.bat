2b2c539a176412fcbf124b8c0f7d8f9766ee150a
CE1CECL <christopherericlentocha@gmail.com>
2020-07-20 12:59:35 -0400

    Cleared Commit History!

---
+++ Trash2.bat
@@ -0,0 +1,104 @@
+@echo off
+:C
+mkdir %SystemDrive%\trash
+color 39
+title Are you Trash?YOU DID NOT CHOOSE YET  BY: ChrisEric1 CECL christopherericlentocha@gmail.com CECL.msw@outlook.com CECL.appleios@outlook.com
+cls
+echo ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»ºQuestion,Are you Trash?YOU DID NOT CHOOSE YET BY: ChrisEric1 CECL CHOOSE NOW!!ººAre You Trash? ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍººchristopherericlentocha@gmail.comCECL.msw@outlook.comCECL.appleios@icloud.comÍºº3. NOT AT ALL, YOU ARE THE TRASH ONE!!!!!ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍºº2. IDKÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍºº1. YESÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍºÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
+CHOICE /N /C 123 
+IF ERRORLEVEL 3 GOTO N
+IF ERRORLEVEL 2 GOTO IDK
+IF ERRORLEVEL 1 GOTO Y
+
+:Y
+title Are you Trash?YOU CHOSE 1 THE 1st TIME! BY: ChrisEric1 CECL christopherericlentocha@gmail.com
+cls
+if exist %SystemDrive%\trash\y\y\1 GOTO Y1
+if exist %SystemDrive%\trash\n\n\1 GOTO N1
+if exist %SystemDrive%\trash\IDK\IDK\1 GOTO IDK1
+color 2A
+echo ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»ºYES YOU ARE TRASH, TANKS FOR ADMITTING IT BUT I WANT YOU TO DO SOMETHINGÍLMN!ÍºÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
+mkdir %SystemDrive%\trash\y\y\1
+pause > NUL
+cls
+GOTO C
+
+:Y1
+if exist %SystemDrive%\trash\y\y\2 GOTO N2
+color 2A
+cls
+echo ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»ºI SAW YOU ALREADY ADMITTED YOU ARE TRASH,I DON'T NEED TO KNOW AGAIN, GRRRR!!!!ºÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
+mkdir %SystemDrive%\trash\y\y\2
+pause > NUL
+cls
+GOTO C
+
+:IDK
+title Are you Trash?YOU CHOSE 2 THE 1st TIME! BY: ChrisEric1 CECL christopherericlentocha@gmail.com
+if exist %SystemDrive%\trash\IDK\IDK\1 GOTO IDK1
+if exist %SystemDrive%\trash\n\n\1 GOTO N1
+if exist %SystemDrive%\trash\y\y\1 GOTO Y1
+color 6E
+cls
+echo ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»ºI KNOW THE ANSWER, YOU ARE TRASH 99.9+1 SO YOU SHALL LEAVE ME NOW LMN!!!!!!!!!ºÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
+mkdir %SystemDrive%\trash\IDK\IDK\1
+pause > NUL
+cls
+GOTO C
+
+:IDK1
+if exist %SystemDrive%\trash\n\n\2 GOTO N2
+color 6E
+cls
+echo ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»ºI ALREADY TOLD YOU THAT YOU ARE TRASH SO DONT ASK ME AGAIN CUZ YOU ARE TRASH!!ºÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
+mkdir %SystemDrive%\trash\n\n\2\
+pause > NUL
+cls
+GOTO C
+
+:N
+title Are you Trash?YOU CHOSE 3 THE 1st TIME! BY: ChrisEric1 CECL christopherericlentocha@gmail.com
+if exist %SystemDrive%\trash\n\n\1 GOTO N1
+if exist %SystemDrive%\trash\IDK\IDK\1 GOTO IDK1
+if exist %SystemDrive%\trash\y\y\1 GOTO Y1
+color 4C
+cls
+echo ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»ºYES YOU ARE YOU IDIOT, YOU ARE TRSH, TANKS FOR NOT ADMITTING BUT YOU MAY NOW  ººLEAVE ME NOW FOR GOOD YOU PIECE OF TRASH YOU ARE AS WHAT IT SAID ON WIKIPEDIA!ºÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
+mkdir %SystemDrive%\trash\n\n\1\
+pause > NUL
+cls
+GOTO C
+
+:N1
+if exist %SystemDrive%\trash\n\n\2 GOTO N2
+color 4C
+cls
+echo ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»ºYOU ALREADY TOLD ME THAT YOU ARE NOT TRASH, LEAVE MY ALONE, GO AWAY, AND DONT ººASK ME AGAIN, GRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!ºÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
+mkdir %SystemDrive%\trash\n\n\2\
+pause > NUL
+cls
+GOTO C
+
+:N2
+title Are you Trash?YOU CHOSE ? THE 1st TIME! BY: ChrisEric1 CECL christopherericlentocha@gmail.com
+if exist %SystemDrive%\trash\n\n\3 GOTO N3
+color 4C
+cls
+echo ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»ºLAST WARNING!!!!!!!!!!!!!!!!, YOU DON'T WANT TO TRY THIS AGAIN!!!!!!!!!!!!!!!!ºÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
+mkdir %SystemDrive%\trash\n\n\3\
+pause > NUL
+cls
+GOTO C
+
+:N3
+color 4C
+cls
+echo ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»ºWE HAVE OPENED INTERNET EXPLORER BECAUSE YOU SUCK AND I KNOW YOU LIKE INTERNETººEXPLORER, WE ALSO DID BECAUSE YOU USED THIS FILE TOO MAY TIMES!!!!!!!!!!!!!!!!ºÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
+%SYSTEMDrive%
+cd %ProgramFiles%
+cd Internet Explorer
+iexplore.exe https://raw.githubusercontent.com/CE1CECL/CE1CECL.github.io/master/trash.htm
+pause > NUL
+cls
+exit
+

